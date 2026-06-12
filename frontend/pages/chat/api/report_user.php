6b153a5770ab80398255d883b17bedd642e5cdc6
Ndaedzo Mudau <ndaedzomu101@gmail.com>
2026-06-12 08:35:08 +0200

    bug fix

--- frontend/pages/chat/api/report_user.php
+++ frontend/pages/chat/api/report_user.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../../../config/bootstrap.php';
 require_once __DIR__ . '/../../../utils/protected_route.php';
-require_once __DIR__ . '/../../../lib/services/Report_service.php'
+require_once __DIR__ . '/../../../lib/services/Report_service.php';
 
 use Lib\services\Report_service;
 
